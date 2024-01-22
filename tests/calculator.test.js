4da1725b3ee123e41cda2c0fb9c06a86853f108f
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-01-22 13:02:25 -0600

    🧪 TEST / Verify the operation of arithmetic operations

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,21 @@
-const { default: expect } = require('expect');
+const calculator = require("../calculator");
 
-const calculator = require('../calculator');
+test("It should add 1 + 2 to equal 3", () => {
+    expect(calculator.sum(1, 2)).toBe(3);
+});
 
-test('adds a + b', () => {
-    //
-})
+test("It should subtract 3 - 2 to equal 1", () => {
+    expect(calculator.substract(3, 2)).toBe(1);
+});
 
-test('substracts a - b', () => {
-   //
-})
+test("It should multiply 5 * 2 to equal 10", () => {
+    expect(calculator.multiply(5, 2)).toBe(10);
+});
 
-test('multiplies a * b', () => {
-    //
-})
+test("It should divide 10 / 5 to equal 2", () => {
+    expect(calculator.divide(10, 5)).toBe(2);
+});
 
-test('divides a / b', () => {
-    // Consider decimals
-})
-
-test('Raises to power a ^ b', () => {
-    //
-})
-
+test("It should return 2 to the power of 2 to equal 4", () => {
+    expect(calculator.power(2, 2)).toBe(4);
+});
